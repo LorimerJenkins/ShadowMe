30f974ed866d12ccd46a1dd84e982a437cc99289
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-10-25 20:35:46 -0400

    -

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -1,44 +1,21 @@
+// postData.ts
 import axios, { AxiosResponse } from 'axios';
 
-interface MessagePayload {
-  contract_id: string;
-  function: string;
-  data: {
+interface PostDataPayload {
+    function: string;
     recipientId: string;
     senderId: string;
-    messageContent: string;
-  };
+    messageContent?: string;
 }
 
-async function sendMessage(payload: MessagePayload): Promise<AxiosResponse> {
-    
-  const { contract_id, function: fn, data } = payload;
+export async function postDataToNetlify(payload: PostDataPayload): Promise<AxiosResponse> {
 
-  try {
-    const response = await axios.post('https://your-api-endpoint-here.com', {
-      function: fn,
-      data
-    });
+    const endpoint = 'https://shadow-me.netlify.app/.netlify/functions/route';
 
-    return response;
-  } catch (error) {
-    throw new Error(`Error sending message: ${error}`);
-  }
+    try {
+        const response = await axios.post(endpoint, payload);
+        return response;
+    } catch (error) {
+        throw error;
+    }
 }
-
-// Example usage:
-const payload: MessagePayload = {
-  contract_id: 'some_id',
-  function: 'some_function',
-  data: {
-    recipientId: 'recipient_123',
-    senderId: 'sender_456',
-    messageContent: 'Hello!'
-  }
-};
-
-sendMessage(payload).then(response => {
-  console.log(response.data);
-}).catch(error => {
-  console.error(error.message);
-});
